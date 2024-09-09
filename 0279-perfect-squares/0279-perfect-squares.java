d5146a534fd4600f3d41729d7ffdcc5593ad3c03
aryantodi13 <65720730+aryantodi13@users.noreply.github.com>
2024-09-09 16:39:21 +0530

    Time: 17 ms (90.84%), Space: 42.7 MB (65.51%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,42 +1,31 @@
 class Solution {
-      public int numSquares(int n){
-        if (n < 4) return n;
-        ArrayList<Integer> s = new ArrayList<>();
-        for(int i = 1; i < n; i ++){
-            int temp = i * i;
-            if(temp > n) break;
-            s.add(i*i);
+     public int numSquares(int n){
+        if(n < 4) return n;
+        ArrayList<Integer> h = new ArrayList<>();
+        for(int i = 1; i < n;i ++){
+            if(i * i <= n) h.add(i*i);
+            else break;
         }
-        Integer[] s2 = new Integer[s.size()];
-        s2 = s.toArray(s2);
-        Integer[][] dp = new Integer[s2.length][n + 1];
-        for(int i = 0; i < s2.length; i ++) Arrays.fill(dp[i], -1);
-        int minCount = dpf(n, s2, s2.length - 1, dp);
-        return minCount;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        int mc = dpf(n, h, dp, h.size() - 1);
+        System.out.println(mc);
+        return mc;
     }
-    public int dpf(int n, Integer[] s, int i, Integer[][] dp){
-        if (i == 0) return n;
-        if(dp[i][n] != -1) return dp[i][n];
-        if (n < s[i]){
-            while(s[i] > n) i--;
-            return dpf(n, s, i, dp);
+    public int dpf(int n, ArrayList<Integer> h, int[] dp, int index){
+        if(h.size() == 1) return n;
+        if(dp[n] != -1) return dp[n];
+        if(h.get(index) > n) {
+            while (h.get(index) > n){
+                index --;
+            }
+            return dpf(n, h, dp, index);
         }
-        if(n - s[i] == 0) return 1;
         else{
-            int temp = n - s[i];
+            if(n % h.get(index) == 0){ return dp[n] = n/h.get(index);}
+            int take = n / h.get(index) + dpf(n % h.get(index), h, dp, index);
+            int notake = dpf(n, h, dp, index - 1);
+            return dp[n] = Math.min(take, notake);
         }
-        // Recursive cases
-        // Take
-        int cTake;
-        if(n % s[i] == 0){
-            cTake = n/s[i];
-        }
-        else {
-            cTake = (Integer) n / s[i] + dpf(n % s[i], s, i - 1, dp);
-        }
-        int cNoTake = dpf(n, s, i - 1, dp);
-        dp[i][n] = Math.min(cTake, cNoTake);
-        return dp[i][n];
-        // No Take
     }
 }
