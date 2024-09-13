ae2402d0bbe8ed857f5353694fc6269aa9b92238
aryantodi13 <65720730+aryantodi13@users.noreply.github.com>
2024-09-13 15:29:31 +0530

    Time: 4 ms (31.19%), Space: 57.8 MB (38.53%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -1,14 +1,18 @@
 class Solution {
-        public int[] xorQueries(int[] arr, int[][] queries) {
+    public int[] xorQueries(int[] arr, int[][] queries) {
         int[] ans = new int[queries.length];
-        for(int i = 0; i < queries.length; i ++){
+        int[] prefix = new int[arr.length + 1];
+        for(int i = 0; i < arr.length; i ++){
+            if(i == 0) prefix[i] = arr[i];
+            else prefix[i] = arr[i] ^ prefix[i-1];
+        }
+        for(int i = 0; i < queries.length; i++ ){
             int leftIndex = queries[i][0];
             int rightIndex = queries[i][1];
-            int xor = arr[leftIndex];
-            for(int j = leftIndex + 1; j <= rightIndex; j ++){
-                xor = xor ^ arr[j];
-            }
-            ans[i] = xor;
+            if(leftIndex == rightIndex) ans[i] = arr[leftIndex];
+            else if(rightIndex - leftIndex == 1) ans[i] = arr[leftIndex] ^ arr[rightIndex];
+            else if (leftIndex == 0) ans[i] = prefix[rightIndex];
+            else ans[i] = prefix[leftIndex - 1] ^ prefix[rightIndex];
         }
         return ans;
     }
