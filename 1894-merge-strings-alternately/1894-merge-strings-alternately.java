c879e7143bc34250f4154c3c8fb5588ad67e0111
aryantodi13 <65720730+aryantodi13@users.noreply.github.com>
2024-09-14 11:54:11 +0530

    Time: 7 ms (20.92%), Space: 42.2 MB (31.49%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,32 +1,20 @@
 class Solution {
-    public String mergeAlternately(String word1, String word2) {
+        public String mergeAlternately(String word1, String word2) {
         String s = "";
-        if(word1.length() == word2.length()){
-            for(int i = 0; i < word2.length(); i ++){
-                String s1 = String.valueOf(word1.charAt(i));
-                String s2 = String.valueOf(word2.charAt(i));
-                s = s + s1 + s2;
-            }
-            return s;
+        int len;
+        int i = 0;
+        if(word1.length() < word2.length()) len = word1.length();
+        else len = word2.length();
+        for(i = 0; i < len; i ++){
+            s +="" + word1.charAt(i) + word2.charAt(i);
         }
-        else if(word1.length() > word2.length()){
-            for(int i = 0; i < word2.length(); i ++){
-                String s1 = String.valueOf(word1.charAt(i));
-                String s2 = String.valueOf(word2.charAt(i));
-                s = s + s1 + s2;
-            }
-            s = s.concat(word1.substring(word2.length()));
-            return s;
+        if(i < word1.length()){
+            return s.concat(word1.substring(len));
         }
-        else{
-            for(int i = 0; i < word1.length(); i ++){
-                String s1 = String.valueOf(word1.charAt(i));
-                String s2 = String.valueOf(word2.charAt(i));
-                s = s + s1 + s2;
-            }
-            s = s.concat(word2.substring(word1.length()));
-            return s;
+        else if (i < word2.length()) {
+            return s.concat(word2.substring(len));
         }
+        return s;
     }
 
 }
