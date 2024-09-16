2d705a9474103fa27700e8b7a7401c0211586740
aryantodi13 <65720730+aryantodi13@users.noreply.github.com>
2024-09-16 14:20:14 +0530

    Time: 16 ms (17.11%), Space: 44.7 MB (97.36%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,5 +1,5 @@
 class Solution {
-              public int findMinDifference(List<String> timePoints) {
+                  public int findMinDifference(List<String> timePoints) {
         if(timePoints.size() == 2) return calcMin(timePoints.get(0), timePoints.get(1));
         timePoints.sort(String::compareTo);
         int li = 0;
@@ -14,10 +14,12 @@ class Solution {
     }
     public int calcMin(String a, String b){
         if(a.equals(b)) return 0;
-        int lh = Integer.valueOf(a.split(":")[0]);
-        int lm = Integer.valueOf(a.split(":")[1]);
-        int rh = Integer.valueOf(b.split(":")[0]);
-        int rm = Integer.valueOf(b.split(":")[1]);
+        String[] l = a.split(":");
+        String[] r = b.split(":");
+        int lh = Integer.valueOf(l[0]);
+        int lm = Integer.valueOf(l[1]);
+        int rh = Integer.valueOf(r[0]);
+        int rm = Integer.valueOf(r[1]);
 
         int diff = Math.abs((lh - rh) * 60 + (lm - rm));
         diff = Math.min(diff, 1440 - diff);
