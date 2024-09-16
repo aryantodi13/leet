d5b24a6c3ca38dab852abd66803a7d8219346e90
aryantodi13 <65720730+aryantodi13@users.noreply.github.com>
2024-09-16 14:03:05 +0530

    Time: 130 ms (5.28%), Space: 46.1 MB (30.06%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -0,0 +1,34 @@
+class Solution {
+        public int findMinDifference(List<String> timePoints) {
+        if(timePoints.size() == 2) return calcMin(timePoints.get(0), timePoints.get(1));
+        timePoints.sort((String x, String y) -> {
+            if(Integer.parseInt((x.split(":")[0])) == Integer.parseInt((y.split(":")[0]))) return Integer.parseInt(x.split(":")[1]) - Integer.parseInt(y.split(":")[1]);
+            return Integer.parseInt(x.split(":")[0]) - Integer.parseInt(y.split(":")[0]);
+        });
+        int li = 0;
+        int ri = 1;
+        int minTime = calcMin(timePoints.get(0), timePoints.get(timePoints.size() - 1));
+        Set<String> s = new HashSet<>();
+        s.add(timePoints.get(0));
+        while(ri < timePoints.size() ){
+            if(s.contains(timePoints.get(ri))) return 0;
+            minTime = Math.min(minTime, calcMin(timePoints.get(li), timePoints.get(ri)));
+            s.add(timePoints.get(ri));
+            li++;
+            ri++;
+        }
+        return minTime;
+    }
+    public int calcMin(String a, String b){
+        if(a.equals(b)) return 0;
+        int lh = Integer.valueOf(a.split(":")[0]);
+        int lm = Integer.valueOf(a.split(":")[1]);
+        int rh = Integer.valueOf(b.split(":")[0]);
+        int rm = Integer.valueOf(b.split(":")[1]);
+
+        int diff = Math.abs((lh - rh) * 60 + (lm - rm));
+        diff = Math.min(diff, 1440 - diff);
+        return diff;
+    }
+
+}
