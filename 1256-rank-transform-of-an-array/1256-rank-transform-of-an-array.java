5183406d9ebd3eb14e9eb03eb4e7f9ac0d849d65
aryantodi13 <65720730+aryantodi13@users.noreply.github.com>
2024-10-02 12:20:31 +0530

    Time: 29 ms (50.91%), Space: 62.5 MB (23.39%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -7,9 +7,8 @@ class Solution {
             ArrayList<Integer> l = new ArrayList<>();
             HashMap<Integer, Integer> mp = new HashMap<>();
             for(int i = 0; i < k.length; i ++){
-                int num = k[i];
-                int tempI = i;
-                mp.compute(num, (key, v) -> v == null ? mp.size() : v);
+                if(mp.containsKey(k[i])) continue;
+                mp.put(k[i], mp.size());
             }
             for(int i = 0; i< arr.length; i ++){
                 k[i] = mp.get(arr[i]) + 1;
